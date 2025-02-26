29c00f72658b234675bc273566bf9eaae49ff63f
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-02-26 12:31:34 +0200

    fonts folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Header from "@/components/header";
 import { Toaster } from "@/components/ui/toaster";
 import { Footer } from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
+import { font } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "TastyLife - Food Delivery",
@@ -21,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         <Header />
         <main className="min-h-screen bg-background">{children}</main>
         <Toaster />
---
+++ src/fonts/index.ts
@@ -0,0 +1,2 @@
+import { Inter } from "next/font/google";
+export const font = Inter({ subsets: ["latin"] });
