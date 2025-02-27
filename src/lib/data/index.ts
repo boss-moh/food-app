f239ec5645f8c20223527b9e1245e78d9ca1b84f
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-02-27 11:58:37 +0200

    db init

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,5 +1,13 @@
 import prisma from "../prisma";
 
-export async function getPosts() {
-  return await prisma.post.findMany();
+
+export async function fetchCategories() {
+  try {
+    
+    const categories = await prisma.category.findMany();
+    return categories
+  }
+  catch {
+    throw new Error('Faild to fetch categories')
+  }
 }
