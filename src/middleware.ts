86d28ae51471690e2a6e5ae51d30ddf39e3bd50a
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-03-19 16:26:32 +0200

    try

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import {
   PUBLICE_PATHS,
   // URL_PATHS,
 } from "./constants";
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 // import { NextResponse } from "next/server";
 
 const { auth } = NextAuth(authConfig);
@@ -34,7 +34,8 @@ export default auth((req) => {
   if (isAdminPath && isThemAdmin) {
     return;
   }
-  return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+  return;
+  // return NextResponse.redirect(new URL("/", req.nextUrl.origin));
   // return NextResponse.redirect(new URL(URL_PATHS.NOT_FOUND, req.nextUrl));
   // return NextResponse.redirect(
   //   new URL(URL_PATHS.NOT_FOUND, req.nextUrl.origin)
