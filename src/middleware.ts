d7f1bc76aa9bd6b955b9ffc6014ce3b0594e40e5
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-03-19 20:34:58 +0200

    change in middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,46 +1,41 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth";
-// import {
-//   API_PREFIX,
-//   PROTECTED_PATHS,
-//   PUBLICE_PATHS,
-//   // URL_PATHS,
-// } from "./constants";
-// import { NextResponse } from "next/server";
-// import { NextResponse } from "next/server";
+import {
+  API_PREFIX,
+  PROTECTED_PATHS,
+  PUBLICE_PATHS,
+  URL_PATHS,
+} from "./constants";
+import { NextResponse } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth(() => {
-  return;
-  // const path = req.nextUrl.pathname;
-  // // console.log("path", path);
+export default auth((req) => {
+  const path = req.nextUrl.pathname;
+  // console.log("path", path);
 
-  // const isApiRequest = path.startsWith(API_PREFIX);
-  // if (isApiRequest) return;
+  const isApiRequest = path.startsWith(API_PREFIX);
+  if (isApiRequest) return;
 
-  // const isPublicPath = PUBLICE_PATHS.includes(path);
-  // if (isPublicPath) return;
+  const isPublicPath = PUBLICE_PATHS.includes(path);
+  if (isPublicPath) return;
 
-  // const isLoggin = !!req.auth;
-  // const isProtecedPath = PROTECTED_PATHS.includes(path);
+  const isLoggin = !!req.auth;
+  const isProtecedPath = PROTECTED_PATHS.includes(path);
 
-  // if (isProtecedPath && isLoggin) {
-  //   return;
-  // }
+  if (isProtecedPath && isLoggin) {
+    return;
+  }
 
-  // const isAdminPath = new RegExp(/admin/gi).test(path);
-  // const isThemAdmin = req.auth?.user.role === "ADMIN";
+  const isAdminPath = new RegExp(/admin/gi).test(path);
+  const isThemAdmin = req.auth?.user.role === "ADMIN";
 
-  // if (isAdminPath && isThemAdmin) {
-  //   return;
-  // }
-  // return;
-  // return NextResponse.redirect(new URL("/", req.nextUrl.origin));
-  // return NextResponse.redirect(new URL(URL_PATHS.NOT_FOUND, req.nextUrl));
-  // return NextResponse.redirect(
-  //   new URL(URL_PATHS.NOT_FOUND, req.nextUrl.origin)
-  // );
+  if (isAdminPath && isThemAdmin) {
+    return;
+  }
+  return NextResponse.redirect(
+    new URL(URL_PATHS.NOT_FOUND, req.nextUrl.origin)
+  );
 });
 
 export const config = {
