f99e4dac73269e963755bd5a97a0139b34ced40b
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-03-20 11:01:44 +0200

    without message

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,7 +1,6 @@
-// import { Inter ,Pacifico} from "next/font/google";
-import { Inter } from "next/font/google";
+import { Inter, Pacifico } from "next/font/google";
 export const font = Inter({ subsets: ["latin"], fallback: [""] });
-export const secondFont = Inter({
+export const secondFont = Pacifico({
   subsets: ["latin"],
   weight: ["400"],
 });
